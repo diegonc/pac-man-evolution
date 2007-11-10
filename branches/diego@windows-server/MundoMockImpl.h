3439e58cde1e6e932db5462ff6c7592df6cf1202
dnietoc <dnietoc@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-11-10 02:14:19 +0000

    Implementación boba de Mundo.

---
+++ branches/diego@windows-server/MundoMockImpl.h
@@ -0,0 +1,28 @@
+﻿#ifndef __MUNDOMOCKIMPL_H__
+#define __MUNDOMOCKIMPL_H__
+
+#include <list>
+#include "Mundo.h"
+#include "Mapa.h"
+
+class MundoMockImpl : public Mundo
+{
+	private:
+		MundoMockImpl();
+		~MundoMockImpl();
+
+		static Mundo* instance;
+
+	public:
+		static MundoMockImpl& get_instance();
+		static void destroy();
+		
+	private:
+		std::list<Mapa*> mapas;
+		std::list<Mapa*>::iterator cursor;
+		
+		Mapa* get_mapa_activo();
+};
+
+#endif /* __MUNDOMOCKIMPL_H__ */
+ 
