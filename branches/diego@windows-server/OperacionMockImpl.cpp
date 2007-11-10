1f0849c0f61c2fc87e4f379027546bcf936ae0cc
dnietoc <dnietoc@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-11-10 13:57:56 +0000

    Implementación de una operación de prueba para probar interfaz Mundo y Mapa.

---
+++ branches/diego@windows-server/OperacionMockImpl.cpp
@@ -0,0 +1,10 @@
+﻿#include "OperacionMockImpl.h"
+#include "MundoMockImpl.h"
+#include "Mapa.h"
+
+void OperacionMockImpl::ejecutar()
+{
+	Mapa& mapa = MundoMockImpl::get_instance().get_mapa_activo();
+	
+	mapa.mover( 1, Mapa::Norte );
+}
---
+++ branches/diego@windows-server/OperacionMockImpl.h
@@ -0,0 +1,12 @@
+﻿#ifndef __OPERACIONMOCKIMPL_H__
+#define __OPERACIONMOCKIMPL_H__
+
+#include "Operacion.h"
+
+class OperacionMockImpl : public Operacion
+{
+	public:
+		void ejecutar();
+};
+
+#endif /* __OPERACIONMOCKIMPL_H__ */
