8d685bc229a992421825f43efa086c79cdd959e3
dnietoc <dnietoc@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-11-10 14:08:35 +0000

    Interfaz inicial de Mapa

---
+++ branches/diego@windows-server/Mapa.h
@@ -0,0 +1,17 @@
+﻿#ifndef __MAPA_H__
+#define __MAPA_H__
+
+class Mapa
+{
+	public:
+		enum Direccion {
+			Norte,
+			Este,
+			Sur,
+			Oeste
+		};
+		
+		void mover( int id, Direccion dir ) = 0;
+};
+
+#endif /* __MAPA_H__ */
