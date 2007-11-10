99f8d5df04396958e334803d38f30047929e17f3
dnietoc <dnietoc@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-11-10 02:13:27 +0000

    Interfaz inicial de Mundo.

---
+++ branches/diego@windows-server/Mundo.h
@@ -0,0 +1,16 @@
+﻿#ifndef __MUNDO_H__
+#define __MUNDO_H__
+
+#include "Mapa.h"
+
+class Mundo
+{
+	private:
+		Mundo();
+		~Mundo();
+
+	public:	
+		Mapa* get_mapa_activo() = 0;
+};
+
+#endif /* __MUNDO_H__ */
