cd079b478e0f603f77a8f7737a3eeb97f89847aa
Diego Nieto Cid <diegonc@users.noreply.github.com>
2007-11-11 21:56:46 +0000

    Documentacion de Mapa.h.

--- server/Mapa.h
+++ server/Mapa.h
@@ -3,13 +3,29 @@
 
 #include "Personaje.h"
 
+/** @brief Interfaz de acceso al mapa.
+ *
+ *  La dispocision de pasillos, conjunto de personajes y los objetos con los
+ *  que interactuan conforman un mapa.
+ *  La interfaz Mapa se define para permitir el acceso a estor elementos. 
+ */
 class Mapa
 {
 	public:
 		virtual ~Mapa() {}
 
+		/** @brief Mueve un personaje.
+		 *
+		 *  @param id  Identificador numerico del personaje.
+		 *  @param dir Direccion en la que se mueve el personaje.
+		 */
 		virtual void mover( int id, Direccion dir ) = 0;
 
+		/** @brief Obtiene un personaje.
+		 *
+		 *  @param id Identificador del personaje a obtener.
+		 *  @return Se devuelve una referencia al personaje.
+		 */
 		virtual Personaje& get_personaje( int id ) = 0;
 };
 
