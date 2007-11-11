21a79ed924eb0b7214d1ec855d1c98f12862cdbc
dnietoc <dnietoc@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-11-11 23:10:59 +0000

    Documentacion de Codec.h.

--- trunk/server/Codec.h
+++ trunk/server/Codec.h
@@ -5,6 +5,11 @@
 #include "Paquete.h"
 #include "../common/smart_pointer.h"
 
+/** @brief Codificador y decodificador de operaciones.
+ *
+ *  Realiza la traduccion entre paquetes provenientes de la red y operaciones
+ *  aplicables al modelo.
+ */
 class Codec
 {
 	private:
@@ -15,7 +20,19 @@ class Codec
 	public:
 		static S_ptr<Codec> get_instancia();
 		~Codec();
+		
+		/** @brief Transforma paquetes en operaciones.
+		 *
+		 *  @param pkt Paquete a traducir.
+		 *  @return Retorna una nueva operacion.
+		 */
 		Operacion* procesar( Paquete* pkt );
+
+		/** @brief Transforma operaciones en paquetes.
+		 *
+		 *  @param op Operacion a traducir.
+		 *  @return Retorna un nuevo paquete.
+		 */
 		Paquete* procesar( Operacion* op );
 };
 
