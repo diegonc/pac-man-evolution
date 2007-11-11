f84e948cf713b54e1fdab3064616dd8b6c38c4fe
dnietoc <dnietoc@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-11-11 22:48:58 +0000

    Documentacion de Operacion.h.

--- trunk/server/Operacion.h
+++ trunk/server/Operacion.h
@@ -1,9 +1,13 @@
 #ifndef __OPERACION_H__
 #define __OPERACION_H__
 
+/** @brief Interfaz de operaciones sobre el modelo.
+ */
 class Operacion
 {
 	public:
+		/** @brief Ejecuta la operacion.
+		 */
 		virtual void ejecutar() = 0;
 	
 		virtual ~Operacion(){}
