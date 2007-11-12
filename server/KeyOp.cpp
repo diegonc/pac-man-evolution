1f633a67f314cfb3e2bbf21c103b58b43a4b4a8a
Diego Nieto Cid <diegonc@users.noreply.github.com>
2007-11-12 00:06:45 +0000

    Documentacion de KeyOp.

--- server/KeyOp.cpp
+++ server/KeyOp.cpp
@@ -4,7 +4,13 @@
 #include "Direccion.h"
 #include "Personaje.h"
 #include "../common/smart_pointer.h"
-
+/**
+ *  La ejecucion de la operacion lleva a cabo los siguientes pasos:
+ *    -# Obtiene jugador segun id.
+ *    -# Obtiene su direccion.
+ *    -# Calcula la nueva direccion.
+ *    -# Mueve el personaje.
+ */
 void KeyOp::ejecutar()
 {
 	/* TODO: Si bien se usa la interfaz se debe conocer la implementacion
@@ -12,12 +18,6 @@ void KeyOp::ejecutar()
 	 */
 	S_ptr<Mundo> mundo = MundoBasicImp::get_instancia();
 	Mapa& mapa = mundo->get_mapa_activo();
-	/* Por ahora:
-	 *   1. Pide personaje segun id.
-	 *   2. Obtiene su direccion.
-	 *   3. Calcula la nueva direccion.
-	 *   4. Mueve el personaje.
-	 */
 	Personaje& p = mapa.get_personaje( id );
 	Direccion d = p.get_direccion().combinar( flecha );
 	mapa.mover( id, d );
--- server/KeyOp.h
+++ server/KeyOp.h
@@ -3,29 +3,29 @@
 
 #include "Operacion.h"
 
+/** @brief Operacion para mover jugadores.
+ *
+ */
 class KeyOp : public Operacion
 {
 	private:
-		/* Id de cliente que presiona la tecla. */
+		/** @brief Id de cliente que presiona la tecla. */
 		int id;
 
-		/* Tecla presionada
+		/** @brief Tecla presionada
 		 *
 		 * Por ahora un entero:
 		 *    0: Flecha arriba
 		 *    1: Flecha abajo
 		 *    2: Flecha izquierda
 		 *    3: Flecha derecha
-		 *
-		 * Dada una direccion inicial, segun han sido definidas en Mapa.h, la
-		 * direccion resultante se calcula:
-		 *         DF = ( DI + Flecha ) % 4
 		 */
 		int flecha;
 	
 	public:
 		KeyOp( int id, int flecha ) : id( id ), flecha( flecha ) {}
 		
+		/** @brief Implementa los efectos de la operacion. */
 		void ejecutar();
 };
 
