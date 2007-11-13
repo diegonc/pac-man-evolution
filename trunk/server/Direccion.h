e4eabc1fa364ec8d95c43ebbfb8d2fc6e31a6a42
dnietoc <dnietoc@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-11-13 22:00:03 +0000

    Documentacion de Direccion.

--- trunk/server/Direccion.h
+++ trunk/server/Direccion.h
@@ -1,6 +1,11 @@
 #ifndef __DIRECCION_H__
 #define __DIRECCION_H__
 
+/** @brief Define las direccion permitidas para un jugador.
+ *  
+ *  Las direcciones Norte, Sur, Este, Oeste se definen como atributos de clase.
+ *  El objeto es inmutable.
+ */
 class Direccion
 {
 	private:
@@ -16,6 +21,16 @@ class Direccion
 		int dir;
 
 	public:
+		/** @brief Cambia de direccion i veces.
+		 *
+		 *  Realiza un numero |i| de giros en sentido horario si i>0 o
+		 *  antihorario si i<0, a partir de la direccion actual.
+		 *  Los giros son unicamente de 90º, resultando en direcciones finales
+		 *  contenidas en el conjunto { Norte, Sur, Este, Oeste }.
+		 *
+		 *  @param i Numero de giros.
+		 *  @return Direccion resultante.
+		 */
 		Direccion combinar( int i );
 };
 
