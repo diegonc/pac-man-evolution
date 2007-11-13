88190b2c7a3d76a6429a7b56bec2c795b0d8b224
alverbner <alverbner@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-11-13 19:43:35 +0000

    modif

--- trunk/server/Cliente.h
+++ trunk/server/Cliente.h
@@ -12,12 +12,16 @@
 #include "Paquete.h"
 #include "../common/smart_pointer.h"
 
+
 class Cliente{
 	typedef unsigned int Tipo_Id;
+	typedef S_ptr<Personaje> Tipo_Personaje;
 	
 	private:
 		Socket_Cliente * socket;
 		Tipo_Id id;
+		Tipo_Personaje personaje;
+	
 	public:
 		
 		/** @brief Constructor de la clase
@@ -25,7 +29,7 @@ class Cliente{
 		 *  @param id Id del cliente
 		 *  @param socket Socket por el cual establece la comunicacion
 		 */
-		Cliente(Tipo_Id id, Socket_Cliente * socket):id(id), socket(socket){}
+		Cliente(Tipo_Id id, Socket_Cliente * socket, S_ptr<Personaje> personaje):id(id), socket(socket), personaje(personaje){}
 		
 		/** @brief Devuelve el id del Cliente
 		 *
@@ -33,16 +37,30 @@ class Cliente{
 		 */	
 		Tipo_id get_id();
 			
-		void enviar_mensaje(S_ptr<Paquete> paquete/*TODO no se si el paquete y las operacones pegan aca*/);
+		/*TODO no se si el paquete y las operacones pegan aca*/
+		
+		void enviar_mensaje(S_ptr<Paquete> paquete);
 			
 		S_ptr<Paquete> recibir_mensaje();
-					
+
+
+		/** @brief Devuelve el personaje del cliente.
+		 *
+		 *  @return id  El personaje del cliente.
+		 */
+		Tipo_Personaje get_personaje();
+			
+		/** @brief Asigna un personaje nuevo al cliente.
+		 *
+		 *  @param id  Personaje que se desee asignar.
+		 */
+		void set_personaje(Tipo_Personaje personaje);
+		
+		/** @brief Implementacion de thread.
+		 *
+		 */
 		void run();
-	
-		
-		
-		
-	
+
 };
 
 
