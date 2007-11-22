6506bc3a1df9ef50c591fe4c505627d4a848f5a0
Diego Nieto Cid <diegonc@users.noreply.github.com>
2007-11-22 15:12:23 +0000

    Tipo de paquete.
    Agregue el tipo a la clase base, con la idea de usarlo para la creacion de
    paquetes.

---
+++ server/Paquete.cpp
@@ -0,0 +1,14 @@
+#include "Paquete.h"
+
+Paquete::Paquete()
+{
+}
+
+Paquete::~Paquete()
+{
+}
+
+char Paquete::get_tipo()
+{
+	return tipo;
+}
--- server/Paquete.h
+++ server/Paquete.h
@@ -6,8 +6,21 @@ class Paquete
 	private:
 		Paquete();
 		Paquete(Paquete &p);
+
+		/** @brief Tipo de paquete.
+		 *
+		 *  Valor numerico que identifica de que tipo es el paquete.
+		 *
+		 *  La idea seria que se lo pueda utilizar para crear el
+		 *  tipo de paquete correspondiente.
+		 *  Por ejemplo, tenindo un arreglo de funciones:
+		 *        Paquete* p = a[ tipo ]();
+		 */
+		/*TODO: enum? solo algun typedef y despues PaqueteEspecifico::ID?
+		 */char tipo;
 	public:
-		
+		char get_tipo();
+
 		//algo similar a esto, que pase a bits
 	
 		/*Tipo*/void serialize();
