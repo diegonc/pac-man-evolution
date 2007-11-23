35ccacfcef503fa88d964c21e68811f1e3397415
alverbner <alverbner@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-11-23 15:20:19 +0000

    modif

--- trunk/server/MapaImp.cpp
+++ trunk/server/MapaImp.cpp
@@ -1,21 +1,36 @@
 #include "MapaImp.h"
 #include <list>
 
+MapaImp::MapaImp(Tipo_Dimensiones ancho, Tipo_Dimensiones alto){
+	//TODO revisar esto como para que no bardeen
+
+	this->ancho = ancho;
+	this->alto = alto;	
+}
+
 MapaImp::~MapaImp()
 {
 
 }
 
-void MapaImp::mover( int id, Direccion dir )
+void MapaImp::mover( S_ptr<Jugador> jugador, Direccion dir )
 {
 
 }
-
-S_ptr<Jugador> MapaImp::get_jugador( int id )
+virtual S_ptr<Comestible> get_comestible(Tipo_Coordenada pos_x, Tipo_Coordenada pos_y){
+	
+}
+/*S_ptr<Jugador> MapaImp::get_jugador( int id )
 {
 	
 }
 
-S_ptr<Jugador> MapaImp::get_jugador(int pos_x, int pos_y){
+S_ptr<Jugador> MapaImp::get_jugador(Tipo_Coordenada pos_x, Tipo_Coordenada pos_y){
 
+}*/
+MapaImp::Tipo_Dimensiones get_ancho(){
+	return this->ancho;
+}
+Tipo_Dimensiones get_alto(){
+	return this->alto;
 }
