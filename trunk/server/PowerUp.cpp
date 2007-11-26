3930881b0927d303c555b11cf4c6afe4f9b04f47
alverbner <alverbner@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-11-26 14:43:40 +0000

    modif

--- trunk/server/PowerUp.cpp
+++ trunk/server/PowerUp.cpp
@@ -1,9 +1,18 @@
-#include "PowerUp.cpp"
+#include "PowerUp.h"
+#include <typeinfo>
+#include "PacMan.h"
+#include <stdexcept>
 
 PowerUp::PowerUp():Comestible(PUNTOS_POWER_UP){}
 		
 PowerUp::~PowerUp(){}
 		
 void PowerUp::afectar_personaje(Personaje& personaje){
-	personaje->set_power_up(true);
+	try{
+		PacMan pacman = dynamic_cast< PacMan& >(personaje);
+		pacman.set_power_up(true);
+	}
+	catch(std::bad_cast &e){/*No hago nada*/}
+	
+		
 }
