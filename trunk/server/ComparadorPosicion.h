30644f22660229af3bd2a9ac38d7d983e2601375
alverbner <alverbner@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-11-30 17:31:17 +0000

    no comment

--- trunk/server/ComparadorPosicion.h
+++ trunk/server/ComparadorPosicion.h
@@ -1,13 +1,15 @@
 #ifndef __COMPARADOR_POSICION_H__
 #define __COMPARADOR_POSICION_H__
 
+#include <math.h>
 #include <iostream>
 	
 class ComparadorPosicion{
 	
 	public:
 		bool operator()(const Posicion &e1, const Posicion &e2){
-			if( ( (int)e1.get_x() == (int)e2.get_x() ) && ((int)e1.get_y() == (int)e2.get_y()) )
+						
+			if( ( floor(e1.get_x()) == floor(e2.get_x()) ) && (floor(e1.get_y()) == floor(e2.get_y())) )
 				return true;
 			else
 				return false;
