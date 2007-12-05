01f0d9a33beb6b58989839427c3b5987347e5930
alverbner <alverbner@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-12-05 12:43:49 +0000

    no comment

--- trunk/server/Fantasma.cpp
+++ trunk/server/Fantasma.cpp
@@ -19,38 +19,48 @@ bool Fantasma::comer(Comestible& comestible){
 void Fantasma::colision(Jugador& jugador){
 	//verifica que el personaje con el que colisiono sea uno al cual le afecte,
 	//en este caso, pacman
-	try{
-		//si el jugador no es si mismo, ya que no tiene sentido
-		if(jugador.get_id() != get_jugador().get_id() ){
-			//trata de castear a pacman, si da error, pasa al catch
-			PacMan pacman = dynamic_cast< PacMan& >(*jugador.get_personaje());
-			Tipo_Coordenada distancia;
+	if(this->esta_vivo() && jugador.get_personaje()->esta_vivo() ){
+		try{
+			//si el jugador no es si mismo, ya que no tiene sentido
+			if(jugador.get_id() != get_jugador()->get_id() ){
+				//trata de castear a pacman, si da error, pasa al catch
+				PacMan pacman = dynamic_cast< PacMan& >(*jugador.get_personaje());
+				Tipo_Coordenada distancia;
+				
+				//calcula la distancia entre los dos personajes calculando el modulo
+				// dist = ( ( x0 - x1 )² + ( y0 - y1)² )^ ½
+				distancia = pow( get_jugador()->get_posicion().get_x() - 	
+							 jugador.get_posicion().get_x() , 2);
 			
-			//calcula la distancia entre los dos personajes calculando el modulo
-			// dist = ( ( x0 - x1 )² + ( y0 - y1)² )^ ½
-			distancia = pow( get_jugador().get_posicion().get_x() - 	
-						 jugador.get_posicion().get_x() , 2);
-		
-			distancia += pow( get_jugador().get_posicion().get_y() - 	
-						  jugador.get_posicion().get_y() , 2);
-		
-			distancia = sqrt(distancia);
+				distancia += pow( get_jugador()->get_posicion().get_y() - 	
+							  jugador.get_posicion().get_y() , 2);
 			
-			//si la distancia es menor o igual que la suma de los radios, 
-			//quiere decir que chocaron
-			if(distancia  <= ( this->get_radio() + pacman.get_radio() ) ){
-				if( pacman.tiene_power_up() ){
-					pacman.matar();			
-				}
-				else{
-					if(jugador.get_personaje()->esta_vivo())
-						this->matar();
+				distancia = sqrt(distancia);
+				
+				//si la distancia es menor o igual que la suma de los radios, 
+				//quiere decir que chocaron
+				if(distancia  <= ( this->get_radio() + pacman.get_radio() ) ){
+					if( !pacman.tiene_power_up() ){
+						//pacman.matar();			
+						S_ptr<Personaje> personaje_j1 = get_jugador()->get_personaje();
+						S_ptr<Personaje> personaje_j2 = jugador.get_personaje();
+						get_jugador()->set_personaje(personaje_j2);
+						jugador.set_personaje(personaje_j1);
+						personaje_j1->set_jugador(&jugador);
+						personaje_j1->matar();
+						personaje_j2->set_jugador(get_jugador());
+						
+					}
+					else{
+						if(jugador.get_personaje()->esta_vivo())
+							this->matar();
+					}
 				}
 			}
 		}
+		catch(std::bad_cast &e){/*No hago nada ya que no pasa anda cuando choca
+									con fantasmas*/}
 	}
-	catch(std::bad_cast &e){/*No hago nada ya que no pasa anda cuando choca
-							con fantasmas*/}
 }
 
 Personaje::Enum_Personaje Fantasma::get_tipo(){
--- trunk/server/PacMan.cpp
+++ trunk/server/PacMan.cpp
@@ -11,37 +11,46 @@ PacMan::PacMan(Jugador *jugador):Personaje(jugador,RADIO_PACMAN){
 
 bool PacMan::comer(Comestible& comestible){
 	//incremento los puntos del jugador dependiendo del comestible
-	(this->get_jugador()).incrementar_puntos(comestible.get_puntos());
+	(this->get_jugador())->incrementar_puntos(comestible.get_puntos());
 	//afecto a este personaje
 	comestible.afectar_personaje(*this);
 	//devuelvo true ya que lo comio
 	return true;	
 }
 void PacMan::colision(Jugador& jugador){
-	//si no es el mismo jugador
-	if(jugador.get_id() != get_jugador().get_id() ){
-		Tipo_Coordenada distancia;
-		//calcula la distancia entre los dos personajes calculando el modulo
-		// dist = ( ( x0 - x1 )² + ( y0 - y1)² )^ ½
-		distancia = pow( get_jugador().get_posicion().get_x() - 	
-						 jugador.get_posicion().get_x() , 2);
-		
-		distancia += pow( get_jugador().get_posicion().get_y() - 	
-						  jugador.get_posicion().get_y() , 2);
-		
-		distancia = sqrt(distancia);
-		//si la distancia es menor o igual que la suma de los radios, 
-		//quiere decir que choca
-		if(distancia  <= (this->get_radio() + jugador.get_personaje()->get_radio() ) ){
-			if( this->tiene_power_up() ){
-				if(jugador.get_personaje()->esta_vivo())
-					jugador.get_personaje()->matar();
+	if(this->esta_vivo() && jugador.get_personaje()->esta_vivo() ){
+		//si no es el mismo jugador
+		if(jugador.get_id() != get_jugador()->get_id() ){
+			Tipo_Coordenada distancia;
+			//calcula la distancia entre los dos personajes calculando el modulo
+			// dist = ( ( x0 - x1 )² + ( y0 - y1)² )^ ½
+			distancia = pow( get_jugador()->get_posicion().get_x() - 	
+							 jugador.get_posicion().get_x() , 2);
+			
+			distancia += pow( get_jugador()->get_posicion().get_y() - 	
+							  jugador.get_posicion().get_y() , 2);
+			
+			distancia = sqrt(distancia);
+			//si la distancia es menor o igual que la suma de los radios, 
+			//quiere decir que choca
+			if(distancia  <= (this->get_radio() + jugador.get_personaje()->get_radio() ) ){
+				if( this->tiene_power_up() ){
+					if(jugador.get_personaje()->esta_vivo())
+						jugador.get_personaje()->matar();
+				}
+				else{
+					//this->matar(); <<-------VER DESPUES SI DA O NO
+					//cambio los personajes
+					S_ptr<Personaje> personaje_j1 = get_jugador()->get_personaje();
+					S_ptr<Personaje> personaje_j2 = jugador.get_personaje();
+					get_jugador()->set_personaje(personaje_j2);
+					jugador.set_personaje(personaje_j1);
+					personaje_j1->set_jugador(&jugador);
+					personaje_j2->matar();
+					personaje_j2->set_jugador(get_jugador());
+				}
 			}
-			else{
-				this->matar();			
-			}
-		}
-		
+		}		
 	}	
 }
 void PacMan::set_power_up(bool activado){
