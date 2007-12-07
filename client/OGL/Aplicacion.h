7107e798f5337c67ab2eefdd70e38397415c2f90
alverbner <alanverbner@users.noreply.github.com>
2007-12-07 23:16:13 +0000

    no comment

--- client/OGL/Aplicacion.h
+++ client/OGL/Aplicacion.h
@@ -24,9 +24,9 @@
 #define APP_VERSION	"0.5"
 
 // ResolucionAncho de pantalla
-#define SCREEN_W	800
+#define SCREEN_W	1280//800 <<---ALAN CANSADO
 // ResolucionAlto de pantalla
-#define SCREEN_H	600
+#define SCREEN_H	1024//600
 //profundidad de colores
 #define SCREEN_BPP	32
 
