bbd5ea976ca68e5ebaab199dec671511855f8273
Diego Nieto Cid <diegonc@users.noreply.github.com>
2007-12-08 00:09:34 +0000

    pool

---
+++ server/ClientPool.h
@@ -0,0 +1,22 @@
+#ifndef __CLIENTPOOL_H__
+#define __CLIENTPOOL_H__
+
+#include <list>
+#include "Cliente.h"
+
+class ClientPool
+{
+    std::list<Cliente*> clientes;
+    unsigned int num_orden;
+
+    public:
+        ClientPool();
+
+        ~ClientPool();
+
+        void lanzar_cliente( Socket_Cliente* sock );
+
+        void join_all();
+};
+
+#endif /* __CLIENTPOOL_H__ */
