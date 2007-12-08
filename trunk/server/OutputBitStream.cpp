65569efa57f5a0e3722f5eab3423ac6eec1e4a49
alverbner <alverbner@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-12-08 00:38:22 +0000

    no comment

---
+++ trunk/server/OutputBitStream.cpp
@@ -0,0 +1,102 @@
+#include "OutputBitStream.h"
+
+#include <iostream>
+#include <bitset>
+OutputBitStream::OutputBitStream(){
+	this->buffer = new unsigned char[_DEFAULT_SIZE];
+	this->buffer[0] = 0;
+	this->bit_index = 0;
+	this->buffer_size = _DEFAULT_SIZE;
+	
+}
+OutputBitStream::~OutputBitStream(){
+	delete[] this->buffer;
+}
+void OutputBitStream::append( const unsigned int n, int val, bool grow ){
+	
+	//obtengo el la cantidad de bits disponibles
+	unsigned int bavail = (buffer_size * 8 - bit_index );
+	//si la cantidad es mayor o puede crecer
+	if(  bavail >= n || grow ){
+		//si la cantidad era menor, lo hago crecer
+		if(bavail < n)
+			this->grow(n - bavail);
+		
+		//lo desplazo n bits para meter los nuevos
+		shift_left(n);
+				
+		unsigned int index = 0;
+		
+		//convierto el numero a binario y lo voy almacenando si es 1, 
+		//ya tiene 0 por defecto
+		while( val >= 2 && index < n){
+			if( val % 2 != 0)
+				// desplazo el 1 y hago un OR para guardar un 1
+				(*( buffer + index / 8 )) |= (1 << (index % 8)) ; 
+			val /=2;
+			index++;
+		}
+		if( val == 1 && index != n)
+			(*( buffer + index / 8 )) |= (1 << (index % 8));
+		
+		bit_index += n ;
+	}
+	
+}
+
+void OutputBitStream::skip(){}
+
+void OutputBitStream::grow( unsigned int n ){
+	unsigned int bsize = buffer_size * 8 + n; 
+	
+	if(n > 0){
+		//creo el nuevo buffer
+		unsigned int new_buffer_size = ( bsize % 8==0 ) ? bsize/8 : ( bsize/8 + 1);
+		unsigned char * new_buffer = new unsigned char [ new_buffer_size ];
+		//los copio
+		memcpy(new_buffer, buffer, new_buffer_size - buffer_size);
+		//borro el viejo
+		delete[] this->buffer;
+		//setteo los tamaños
+		buffer_size = new_buffer_size;
+		//setteo el nuevo buffer
+		this->buffer = new_buffer;
+		
+			
+	}
+}
+const unsigned char* OutputBitStream::get_data(){
+	return this->buffer;
+}
+void OutputBitStream::shift_left(const unsigned int n_bits){
+	
+	unsigned int aux;
+	for(int index = bit_index - 1 ; index >= 0; index--){
+		aux = (index + n_bits) / 8 ;
+		if(aux <= buffer_size ){
+			if(	*buffer &  (1 << index % 8) ) //era 1
+				(*( buffer + aux )) |= (  1 << (index + n_bits) % 8) ;
+			else
+				(*( buffer + aux )) &= ~( 1 << (index + n_bits) % 8) ;
+			
+			(*( buffer + index / 8 )) &= ~( 1 << (index) % 8) ;
+		}
+	}
+}
+unsigned int OutputBitStream::get_size(){
+	return buffer_size;
+}
+
+unsigned int OutputBitStream::bits_needed(unsigned int num){
+	
+	unsigned int count = 0;
+	
+	while( num >= 2 ){
+		count++;
+		num /= 2;
+	}
+	count ++;
+		
+	return count;	
+	
+}
