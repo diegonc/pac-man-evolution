1dbf5c38dbaaa77968c83449783f8fff2d8f45ef
dnietoc <dnietoc@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-12-08 13:32:57 +0000

    carpeta para el codigo de red en la parte del cliente.

---
+++ trunk/client/red/Cliente.h
@@ -0,0 +1,18 @@
+#ifndef __CLIENTE_H__
+#define __CLIENTE_H__
+
+#include "../../common/client_socket.h"
+#include <string>
+
+class Cliente : public Thread
+{
+    std::string ip;
+    short port;
+
+    public:
+        Cliente( const std::string& ip, short port );
+
+        void run();
+};
+
+#endif /* __CLIENTE_H__ */
