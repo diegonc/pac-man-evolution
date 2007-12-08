f86c8d0828885b62f0d77f392159d4c16a9017ae
Diego Nieto Cid <diegonc@users.noreply.github.com>
2007-12-08 21:12:26 +0000

    test de obs.

--- tests/obstream/main.cpp
+++ tests/obstream/main.cpp
@@ -25,10 +25,33 @@ void test_2()
 		std::cout << "ERROR: byte 1 es " << (int)raw[0] << " (esperado: 0\n";
 }
 
+void test_3()
+{
+	OutputBitStream obs;
+	obs.append( 2, 0 );
+	obs.append( 3, 0 );
+	obs.append( 3, 1 );
+	obs.append( 8, 3 );
+	const unsigned char* raw = obs.get_data();
+	if( obs.get_size() != 2 )
+		std::cout << "ERROR: tamano " << obs.get_size() << " (esperado: 2 )\n";
+	else {
+		int outs[2] = { 1, 3 };
+		for( int i = 0; i < 2; i++ ) {
+		       	if( raw[i] != outs[i] ) {
+				std::cout << "ERROR: byte " << i << " es ";
+				std::cout << (int)raw[i] << " (esperado: " << outs[i] << ")\n";
+			}
+		}
+	}
+}
+
+
 int main()
 {
 	test_1();
 	test_2();
+	test_3();
 	return 0;
 }
 
