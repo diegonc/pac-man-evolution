f79da8468a2c16a7f793e6d0a6c73cc11bcdfe15
Diego Nieto Cid <diegonc@users.noreply.github.com>
2007-12-08 13:32:57 +0000

    carpeta para el codigo de red en la parte del cliente.

---
+++ client/red/Cliente.cpp
@@ -0,0 +1,30 @@
+#include "client_Cliente.h"
+#include <iostream>
+#include "common_Errores.h"
+
+Cliente::Cliente( const std::string& ip, short port )
+{
+	this->ip = ip;
+	this->port = port;
+}
+
+void* Cliente::run()
+{
+    try {
+	Socket_Cliente sock;
+        sock.conectar( ip, port );
+
+	//TODO: recibir paquetes.
+/*
+    } catch( Socket::ConnectionClosed e ) {
+
+    } catch( Error::SystemError e ) {
+        if( !e.interrumpido() ){
+            // Se no ha sido interrumpido, muestra error.
+            std::cerr << e.what() << std::endl;
+        }
+        error = Error::OTRO;
+    }
+*/  }catch( std::runtime_error e ) {
+    }
+}
