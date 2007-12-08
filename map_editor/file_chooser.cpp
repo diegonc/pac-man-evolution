8f8cc6edcf9166669217ab43a47924c56dea4c2d
rodrigofondato <rfondato@users.noreply.github.com>
2007-12-08 14:35:30 +0000

    (no comment)

--- map_editor/file_chooser.cpp
+++ map_editor/file_chooser.cpp
@@ -8,20 +8,24 @@
 FileChooser::FileChooser(char* titulo, GtkWidget* window, GtkFileChooserAction accion){
 	const char* stock;
 	this->accion = accion;
+	//Segun la accion coloco un boton de guardar o abrir
 	if (accion == GTK_FILE_CHOOSER_ACTION_SAVE)
 		stock = GTK_STOCK_SAVE;
 	else
 		stock = GTK_STOCK_OPEN;
+	//Creo el widget del file chooser para el manejo de archivos con los datos recibidos por parametro
 	this->file_chooser = gtk_file_chooser_dialog_new (titulo, GTK_WINDOW(window), accion,
 				      GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
 				      stock, GTK_RESPONSE_ACCEPT,
 				      NULL);
+	//Pongo que me pida confirmacion para overwrite
 	gtk_file_chooser_set_do_overwrite_confirmation(GTK_FILE_CHOOSER(file_chooser), true);
-	this->filtro_xml = gtk_file_filter_new ();
+	//Creo el filtro para archivos con extension del mundo y le pongo el patron "*.ext_mundo"
+	this->filtro_mundo = gtk_file_filter_new ();
 	string pattern ("*");
 	pattern += EXT_MUNDO;
 	gtk_file_filter_add_pattern(filtro_xml, pattern.c_str());
-	gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(file_chooser), filtro_xml);
+	gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(file_chooser), filtro_mundo);
 }
 
 /* Destructor: */
@@ -34,20 +38,27 @@ FileChooser::~FileChooser(){
 
 void FileChooser::ejecutar(){
 	Mundo* mundo = Modelo::get_instance()->get_mundo();
+	//Corro el dialogo del file chooser, y si la respuesta del usuario es aceptar...
 	if (gtk_dialog_run (GTK_DIALOG (this->file_chooser)) == GTK_RESPONSE_ACCEPT) {
 		char* filename = this->get_filename();
+		//Si la accion es guardar, guardo el mundo segun el filename
 		if (this->accion == GTK_FILE_CHOOSER_ACTION_SAVE){
 			if (!mundo->toXml(filename))
+				//Si se produjo un error guardando el archivo notifico por pantalla
 				this->mostrar_msg("Error guardando archivo.");
 		} else {
+			//Si la accion es abrir, creo un nuevo mundo y lo cargo desde el archivo con nombre filename
 			Mundo* mundo_nuevo = new Mundo();
 			if (mundo_nuevo->fromXml(filename)){
+				//Si se pudo cargar, seteo el nuevo mundo al modelo
 				Modelo::get_instance()->set_mundo(mundo_nuevo);
 			} else {
+				//Sino muestro error y borro el nuevo mundo
 				this->mostrar_msg("Error abriendo archivo.");
 				delete (mundo_nuevo);
 			}
 		}
+		//Libero el filename
 		g_free (filename);
 	}
 }
@@ -64,27 +75,17 @@ char* FileChooser::get_filename(){
 	return gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (this->file_chooser));
 }
 
-/* Cumple Filtro: */
-
-bool FileChooser::cumple_filtro(){
-	char* filename = this->get_filename();
-	GtkFileFilterInfo filter_info;
-	filter_info.filename = filename;
-	filter_info.uri = gtk_file_chooser_get_uri (GTK_FILE_CHOOSER (this->file_chooser));
-	filter_info.display_name = filename;
-	bool result = gtk_file_filter_filter (this->filtro_xml, &filter_info);
-	g_free(filename);
-	return result;
-}
-
 /* Mostrar Msg: */
 
 void FileChooser::mostrar_msg(char* msg){
+	//Creo un dialogo de error con el mensaje recibido por param
 	GtkWidget* dialog = gtk_message_dialog_new (GTK_WINDOW(GUI::get_instance(0,0)->get_ventana_principal()->get_widget()),
 						  GTK_DIALOG_DESTROY_WITH_PARENT,
 						  GTK_MESSAGE_ERROR,
 						  GTK_BUTTONS_CLOSE,
 						  msg);
+	//Corro el dialogo
 	gtk_dialog_run (GTK_DIALOG (dialog));
+	//Lo destruyo
 	gtk_widget_destroy (dialog);
 }
