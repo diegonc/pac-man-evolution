ac7a762cb44729420d619e85920d30ed739596bc
dnietoc <dnietoc@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-12-08 22:51:38 +0000

    constantes de keyop. paqueteinti a  medias.

--- trunk/server/PaqueteInit.cpp
+++ trunk/server/PaqueteInit.cpp
@@ -4,7 +4,13 @@ namespace {
 	const char ID = 0;
 }
 
-PaqueteInit::PaqueteInit( bool pac, MapaBajoNivel& m ) : Paquete( ID ), mapa( m )
+PaqueteInit::PaqueteInit() : Paquete( ID )
+{
+
+}	
+
+PaqueteInit::PaqueteInit( bool pac, MapaBajoNivel& m )
+	: Paquete( ID ), mapa( S_ptr<MapaBajoNivel>( &m ) )
 {
 	esPacman = pac;
 }
@@ -14,39 +20,36 @@ void PaqueteInit::deserialize( InputBitStream& bs )
 	esPacman = ( bs.read( 1 ) == 0 ); // Lectura del rol desde el campo auxiliar.
 	bs.skip(); // Saltea el resto del campo auxiliar.
 
-	grafo_ancho = bs.read( 8 );
-	grafo_alto = bs.read( 8 );
-
-	int long_aristas = grafo_ancho * grafo_alto * 2;
-        
-	grafo_aristas = bs.read_block( long_aristas );
+	int ancho = bs.read( 8 );
+	int alto = bs.read( 8 );
+	int long_aristas = ancho * alto * 2;
+	char* conexiones = bs.read_block( long_aristas );
 	bs.skip();
-
-	// TODO: esto solo tiene sentido en el programa del cliente.
+//	generar_mapa( 
 }
 
 void PaqueteInit::serialize( OutputBitStream& bs )
 {
 	Paquete::serialize( bs ); // Escribe version de protocolo e ID de paquete.
 
-	bs.append( 1, esPacman ); // Escribe campo auxiliar.
+	bs.append( 1, esPacman ? 0 : 1 ); // Escribe campo auxiliar.
 	bs.skip();
 
-	bs.append( 8, mapa.get_ancho() );
-	bs.append( 8, mapa.get_alto() );
+	bs.append( 8, mapa->get_ancho() );
+	bs.append( 8, mapa->get_alto() );
 
-	bs.grow( mapa.get_ancho()*mapa.get_alto()*2 );
+	bs.grow( mapa->get_ancho()*mapa->get_alto()*2 );
 
-	for( unsigned int y=0; y < mapa.get_alto(); y++ ) {
+	for( unsigned int y=0; y < mapa->get_alto(); y++ ) {
 		// Aristas verticales
-		for( unsigned int x=0; x < mapa.get_ancho(); x++ ){
+		for( unsigned int x=0; x < mapa->get_ancho(); x++ ){
 			Posicion p( x, y );
-			bs.append( 1, !( mapa.get_estructural( p )->get_arriba().es_nulo() ) );
+			bs.append( 1, !( mapa->get_estructural( p )->get_arriba().es_nulo() ) );
 		}
 		// Aristas horizontales
-		for( unsigned int x=0; x < mapa.get_ancho(); x++ ) {
+		for( unsigned int x=0; x < mapa->get_ancho(); x++ ) {
 			Posicion p( x, y );
-			bs.append( 1, !( mapa.get_estructural( p )->get_derecha().es_nulo() ) );
+			bs.append( 1, !( mapa->get_estructural( p )->get_derecha().es_nulo() ) );
 		}
 	}
 
