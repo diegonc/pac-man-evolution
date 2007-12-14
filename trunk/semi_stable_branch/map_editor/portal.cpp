89374087810d76ba532f481f42af75dea31e47b3
rodrigofondato <rodrigofondato@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-12-14 15:42:07 +0000

    (no comment)

--- trunk/semi_stable_branch/map_editor/portal.cpp
+++ trunk/semi_stable_branch/map_editor/portal.cpp
@@ -7,7 +7,6 @@
 
 Portal::Portal(int pos_x, int pos_y, int ancho, int alto, Orientacion orientacion) : Estructural(pos_x, pos_y, ancho, alto, orientacion) {
 	this->estoy_agregado = false;
-	this->mapa = NULL;
 }
 
 /* Agregate: */
@@ -17,22 +16,19 @@ bool Portal::agregate(S_ptr<Elemento> instancia, Mapa* mapa){
 	//Si el portal se agrega en el limite del mapa y no esta agregado ya
 	if ((!this->estoy_agregado)
 		&& (this->estoy_en_frontera(mapa))){
-			this->mapa = mapa;
 			//Intento agregar al elemento
-			cout << "asd1" << endl << flush;
 			result = Estructural::agregate(instancia, mapa);
 			//Si se pudo agregar intento agregar al simetrico
 			if (result){
-				cout << "asd2" << endl << flush;
+				this->estoy_agregado = true;
 				result = this->agregar_simetrico(mapa);
-				cout << "asd3" << endl << flush;
 				//Si no se pudo agregar al simetrico quito al elem actual
-				if (!result) {
+				if (!result)
 					this->quitate(instancia, mapa);
-					this->mapa = NULL;
-					cout << "asd4" << endl << flush;
+				else { //Si se encontro al elem simetrico, realizo conexion
+					S_ptr<Elemento> elem_simetrico = this->get_simetrico(mapa);
+					this->conectar_con_simetrico(instancia, elem_simetrico, mapa);
 				}
-				cout << "asd5" << endl << flush;
 			}
 	} else //Sino devuelve falso
 		result = false;	
@@ -49,7 +45,6 @@ void Portal::quitate(S_ptr<Elemento> instancia, Mapa* mapa){
 		//Quito al simetrico del mapa
 		this->quitar_simetrico(mapa);
 		this->estoy_agregado = false;
-		this->mapa = NULL;
 	}
 }
 
@@ -59,27 +54,53 @@ TipoElem Portal::get_tipo(){
 	return PORTAL;
 }
 
-/* Agregar Simetrico: */
+/* Get Simetrico: */
 
-bool Portal::agregar_simetrico(Mapa* mapa) {
-	bool result;
-	//Creo el elem simetrico
-	S_ptr<Elemento> elem_simetrico = this->crear_simetrico(mapa);
-	//Lo agrego al mapa pasado por param
-	result = elem_simetrico->agregate(elem_simetrico, mapa);
-	return result;
-}
-	
-/* Quitar Simetrico: */
-
-void Portal::quitar_simetrico(Mapa* mapa){
+S_ptr<Elemento> Portal::get_simetrico(Mapa* mapa){
 	//Cargo la pos del elem actual en un punto
 	S_ptr<Punto> pos (new Punto(this->get_pos_x(), this->get_pos_y()));
 	//Obtengo la pos simetrica
 	S_ptr<Punto> pos_simetrica = get_pos_simetrica(mapa);
 	//Obtengo el elem simetrico
-	S_ptr<Elemento> elem_simetrico = mapa->get_casillero(pos_simetrica->get_x(), pos_simetrica->get_y())->get_estructural();
-	//Si el elemento no es nulo lo quito del mapa
-	if (!elem_simetrico.es_nulo())
+	S_ptr<Elemento> elem_simetrico = mapa->get_casillero(pos_simetrica->get_x(), pos_simetrica->get_y())->get_estructural();;
+	return elem_simetrico; // Lo devuelvo
+}
+
+/* Agregar Simetrico: */
+
+bool Portal::agregar_simetrico(Mapa* mapa) {
+	bool result;
+	//Pido el elem simetrico
+	S_ptr<Elemento> elem_simetrico = this->get_simetrico(mapa);
+	//Si el simetrico es nulo, lo creo y lo agrego al mapa
+	if (elem_simetrico.es_nulo()) {
+		//Creo el elem simetrico
+		elem_simetrico = this->crear_simetrico(mapa);
+		//Lo agrego al mapa pasado por param. Si se pudo agregar result queda en true.
+		//Sino queda en false.
+		result = elem_simetrico->agregate(elem_simetrico, mapa);
+	} else { //Si hay simetrico
+		//Si es un portal es porque ya estaba agregado
+		if (elem_simetrico->get_tipo() == PORTAL)
+			result = true;
+		else
+			result = false; //Si existia el simetrico pero no es un portal entonces no se pudo agregar
+	}
+	return result;
+}
+
+/* Conectar con Simetrico: */
+
+void Portal::conectar_con_simetrico(S_ptr<Elemento> instancia, S_ptr<Elemento> simetrico, Mapa* mapa){
+		mapa->conectar(instancia, simetrico);
+}
+
+/* Quitar Simetrico: */
+
+void Portal::quitar_simetrico(Mapa* mapa){
+	//Obtengo el elem simetrico
+	S_ptr<Elemento> elem_simetrico = this->get_simetrico(mapa);
+	//Si el elemento no es nulo y es un portal lo quito del mapa
+	if ((!elem_simetrico.es_nulo()) && (elem_simetrico->get_tipo() == PORTAL))
 		elem_simetrico->quitate(elem_simetrico, mapa);
 }
