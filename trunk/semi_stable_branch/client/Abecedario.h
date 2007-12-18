8d2f16cc4c5769911a5485af887979be857afff5
jpbelli <jpbelli@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-12-18 02:15:55 +0000

    no comment

---
+++ trunk/semi_stable_branch/client/Abecedario.h
@@ -0,0 +1,50 @@
+#ifndef ABECEDARIO_H
+#define ABECEDARIO_H
+
+
+
+#include <SDL.h>
+#include "JugadorLocal.h"
+#include "ModeloServidor.h"
+#include <math.h>
+
+
+class Abecedario{
+	//Mapa: 256 x 256
+	SDL_Surface* TextABC;
+	//Mapa: 16 x 16
+	SDL_Surface* TextLetra;
+
+	const static int DimAbecedario=256;
+	const static int DimLetra=16;
+	const static int LetrasXFC= (int) (DimAbecedario/DimLetra);
+	const static int ScreenWidth=800;
+	const static int ScreenHeight=600;
+
+	public:
+
+	Abecedario();
+
+	~Abecedario();
+
+	void ImprimirFrase(SDL_Surface* Screen,std::string Frase);
+
+
+	private:
+
+	void get_letra(char letra);
+
+	void DibujarLetra(SDL_Surface* Screen, int ImgXCurr, int ImgYCurr);
+
+	public:
+
+	void CargarTextura();
+
+	private:
+
+	void CargarSDLSurface(SDL_Surface** Textura,char* Nombre);
+
+
+};
+
+#endif
