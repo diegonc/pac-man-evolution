f96226e4dacb99222928d4814a0c956800f7934a
jpbelli <jpbelli@users.noreply.github.com>
2007-12-18 20:12:20 +0000

    no comment

--- semi_stable_branch/pme-client/Sonidos.h
+++ semi_stable_branch/pme-client/Sonidos.h
@@ -10,22 +10,10 @@ class Sonido{
 	public:
 	Sonido(std::string NombreArch);
 
-	//A continuación cargamos el sonido en dicha varible con la siguiente función:
-
-
-	/*
-	Una vez que hemos cargado el sonido, pasamos a reproducirlo. Tenemos que pasarle 3 parametros:
-	- El canal que le vamos a asignar.
-	- El nombre de la varible donde lo hemos cargado.
-	- El número de veces que queremos que se repita ( 1--> una vez, 2--> dos veces, etc. Si le pasamos -1 se repetira indefinidamente ).
-	*/
 	void Play(int vecesRep);
 
-	//Ahora si este sonido, una vez reproducido, no nos sirve para nada más lo eliminamos de la siguiente forma:
-	//Primero lo paramos ( para asegurarnos ) pasandole como parámetro el canal que anteriormente le habiamos asignado.
 	void Parar();
 
-	// Luego liberamos la variable sonido:
 	~Sonido();
 
 };
