28f7a34f379099ddbb368641354f3e5fd002e72c
jpbelli <jpbelli@440cd090-4c3e-0410-b77e-8701ce9d1544>
2007-12-18 02:08:24 +0000

    no comment

--- trunk/semi_stable_branch/client/AplicacionGrafica.cpp
+++ trunk/semi_stable_branch/client/AplicacionGrafica.cpp
@@ -13,27 +13,28 @@ void AplicacionGrafica::CambiarCamara(){
 //inicializacion de OpenGL
 bool AplicacionGrafica::InitGL(SDL_Surface *S)
 {
-    //color de borrado del la pantalla Negro
-    glClearColor(0.0f,0.0f,0.0f,0.5f);
-    //seteo la profundidad de buffer
-    glClearDepth(1.0f);
-    //tipo de calculo de profundidad (Menor O IGUAL)
-    glDepthFunc(GL_LEQUAL);
-    //linea necesaria para que calculo q objeto se ve sobre q objeto, sino los dibuja en orden de aparicion
-    glEnable(GL_DEPTH_TEST);
-    //seteo el tipo de sombreado
-    glShadeModel(GL_SMOOTH);
-    //habilito el uso de luces
-    //glEnable(GL_LIGHTING);
-    //habilito la luz del jugador
-    //glEnable(GL_LIGHT0);
-    //habilito el uso de texturas en 2D
-    glEnable( GL_TEXTURE_2D );
-    //parametros para la mejor visualizacion de la perspectiva
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-    //escenario.Procesar(); //<----LO COMENTE YOOOOO!!!!
+	//color de borrado del la pantalla Negro
+	glClearColor(0.0f,0.0f,0.0f,0.5f);
+	//seteo la profundidad de buffer
+	glClearDepth(1.0f);
+	//tipo de calculo de profundidad (Menor O IGUAL)
+	glDepthFunc(GL_LEQUAL);
+	//linea necesaria para que calculo q objeto se ve sobre q objeto, sino los dibuja en orden de aparicion
+	glEnable(GL_DEPTH_TEST);
+	//seteo el tipo de sombreado
+	glShadeModel(GL_SMOOTH);
+	//habilito el uso de luces
+	//glEnable(GL_LIGHTING);
+	//habilito la luz del jugador
+	//glEnable(GL_LIGHT0);
+	//habilito el uso de texturas en 2D
+	glEnable( GL_TEXTURE_2D );
+	//parametros para la mejor visualizacion de la perspectiva
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	//escenario.Procesar(); //<----LO COMENTE YOOOOO!!!!
 	//Mapa = SDL_LoadBMP("./MiniMapa/Mapa.bmp");
 	minimapa.CargarTexturas();
+	ABC.CargarTextura();
 	ModeladorOBJ.hidratar();
 	return true;
 
@@ -42,8 +43,8 @@ bool AplicacionGrafica::InitGL(SDL_Surface *S)
 //inicializacion de Datos necesarios para interfase grafica
 bool AplicacionGrafica::Initialize()
 {
-    //Inicializo Informacion de Aplicacion
-    AppEstado.Visible		= true;
+	//Inicializo Informacion de Aplicacion
+	AppEstado.Visible	= true;
 	AppEstado.MouseFocus	= true;
 	AppEstado.KeyboardFocus = true;
 
@@ -53,7 +54,7 @@ bool AplicacionGrafica::Initialize()
 //finalizar y liberar recursos
 void AplicacionGrafica::Deinitialize()
 {
-//  	SDL_FreeSurface(Mapa); 
+
 }
 
 
@@ -92,111 +93,97 @@ void AplicacionGrafica::Draw(SDL_Surface *Screen)
 }
 
 
-//dibujo el modelo 3D
-void AplicacionGrafica::Draw3D(SDL_Surface *S)
-{
-    // Apila la transformación geométrica actual
-    glPushMatrix();
+void AplicacionGrafica::DibujarModelo(){
 	Jugador * jugador;
 	jugador=ModeloServidor::get_instancia()->get_jugador(JugadorLocal::get_instancia()->get_id());
 	static 	double j=0;
 	if (jugador!=NULL){
-	    if (jugador->get_personaje()->esta_vivo()){
+		if (jugador->get_personaje()->esta_vivo()){
 		    //color de borrado del la pantalla Negro
-		glClearColor(0.0f,0.0f,0.0f,0.5f);
-	    }else{
-		j+=0.0001;
-		glClearColor((sin(500*j)+1)/4,0.0f,0.0f,0.5f);
-	    }
+		    glClearColor(0.0f,0.0f,0.0f,0.5f);
+		}else{
+		    j+=0.0001;
+		    glClearColor((sin(500*j)+1)/4,0.0f,0.0f,0.5f);
+		}
 	}
-		
-    //borro pantalla y buffer de profundidad
-    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);		// Clear Screen And Depth Buffer
-    //cambio a la matriz de modelo
-    glMatrixMode(GL_MODELVIEW);
 
-    //reseteo la misma cargando la ident
-    glLoadIdentity();
+	//borro pantalla y buffer de profundidad
+	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);		// Clear Screen And Depth Buffer
+	//cambio a la matriz de modelo
+	glMatrixMode(GL_MODELVIEW);
+
+	//reseteo la misma cargando la ident
+	glLoadIdentity();
 
 
-    	Camara.ejecutar();
+	Camara.ejecutar();
+
+	Posicion_Graf Pos;
+	std::list< Jugador * > lista_jugadores = ModeloServidor::get_instancia()->get_jugadores();
+	std::list< Jugador * >::const_iterator jugadores;
+	Jugador * jp;
+
+	//dibujo al jugador
+	jp=ModeloServidor::get_instancia()->get_jugador(JugadorLocal::get_instancia()->get_id());
+
+	if (jp!=NULL){
+		Posicion pJ=jp->get_posicion();
+		Pos.x=pJ.get_x()*4;
+		Pos.y=-pJ.get_y()*4;
+		Pos.setAnguloActual(getAnguloDireccion(jp->get_direccion()));
+		//dibujo al objeto observador
+		ModeladorOBJ.DibujarObjetoObservadorPosicion(&Pos,jp->get_personaje()->get_tipo());
+	}
+
+	//dibujo los demas
+	for(jugadores = lista_jugadores.begin();jugadores != lista_jugadores.end(); ++jugadores){
+		jp = *jugadores;
+		if(jp->get_id() !=JugadorLocal::get_instancia()->get_id()){
+			Posicion p2=jp->get_posicion();
+			Pos.x= p2.get_x()*4;
+			Pos.y= -p2.get_y()*4;
+			ModeladorOBJ.DibujarObjetoPosicion(&Pos,jp->get_personaje()->get_tipo());
+		}
+	}
+
+	std::list< S_ptr<Comestible> > lista_comestibles = ModeloServidor::get_instancia()->get_mundo().get_mapa_activo()->get_comestibles();
+	std::list< S_ptr<Comestible> >::iterator comestibles;
+	S_ptr<Comestible> comestible;
+
+	//itero sobre los comestibles
+	for(comestibles = lista_comestibles.begin(); comestibles != lista_comestibles.end(); ++comestibles){
+		comestible = *comestibles;
+		Posicion p=comestible->get_posicion();
+		Pos.x= p.get_x()*4;
+		Pos.y= -p.get_y()*4;
+		ModeladorOBJ.DibujarObjetoPosicion(&Pos,comestible->get_tipo());
+	}
+
+	//llamo a la lista precompilada del Escenario
+	escenario.ModelarEscenario();
+}
+
+//dibujo el modelo 3D
+void AplicacionGrafica::Draw3D(SDL_Surface *S)
+{
+	// Apila la transformación geométrica actual
+	glPushMatrix();
 
 
 	if (escenario.loaded()){
-		Posicion_Graf Pos;
-		std::list< Jugador * > lista_jugadores = ModeloServidor::get_instancia()->get_jugadores();
-		std::list< Jugador * >::const_iterator jugadores;
-		Jugador * jp;
-	
-		//dibujo al jugador
-		jp=ModeloServidor::get_instancia()->get_jugador(JugadorLocal::get_instancia()->get_id());
-	
-		if (jp!=NULL){
-			Posicion pJ=jp->get_posicion();
-			Pos.x=pJ.get_x()*4;
-			Pos.y=-pJ.get_y()*4;
-			Pos.setAnguloActual(getAnguloDireccion(jp->get_direccion()));
-			//dibujo al objeto observador
-			ModeladorOBJ.DibujarObjetoObservadorPosicion(&Pos,jp->get_personaje()->get_tipo());
-		}
-		
-		//dibujo los demas
-		for(jugadores = lista_jugadores.begin();jugadores != lista_jugadores.end(); ++jugadores){
-			jp = *jugadores;
-			if(jp->get_id() !=JugadorLocal::get_instancia()->get_id()){
-				Posicion p2=jp->get_posicion();
-				Pos.x= p2.get_x()*4;
-				Pos.y= -p2.get_y()*4;
-				ModeladorOBJ.DibujarObjetoPosicion(&Pos,jp->get_personaje()->get_tipo());
-			}
-		}
-
-		std::list< S_ptr<Comestible> > lista_comestibles = ModeloServidor::get_instancia()->get_mundo().get_mapa_activo()->get_comestibles();
-		std::list< S_ptr<Comestible> >::iterator comestibles;
-		S_ptr<Comestible> comestible;
-
-		//itero sobre los comestibles
-		for(comestibles = lista_comestibles.begin(); comestibles != lista_comestibles.end(); ++comestibles){
-			comestible = *comestibles;
-			Posicion p=comestible->get_posicion();
-			Pos.x= p.get_x()*4;
-			Pos.y= -p.get_y()*4;
-			ModeladorOBJ.DibujarObjetoPosicion(&Pos,comestible->get_tipo());
-		}
-
-		//llamo a la lista precompilada del Escenario
-		escenario.ModelarEscenario();
-//		std::cout << "<---------------------------------------------->" <<std::endl;
-	}else
-        usleep(1000);
+		DibujarModelo();
+	}else{
+		glClearColor(0.0f,0.0f,0.0f,0.5f);
+		//borro pantalla y buffer de profundidad
+		glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);		// Clear Screen And Depth Buffer
+	}
 
 }
 //dibuja en 2D
 void AplicacionGrafica::Draw2D(SDL_Surface *S)
 {
-/*
-	SDL_Rect src1={0,0,0,0};							// We're blitting 3 rectangles,
-
-																// That's an issue many people is having!
-																// We set up our Alpha Channel first!
-
-	src1.x = (Sint16)(800-260);
-																// src1 is the first Logo
-	src1.y = (Sint16)(0);
-
-	src1.w = 260;										// Fill the rect structure
-	src1.h = 260;
-	
-	SDL_FillRect(S, &src1, SDL_MapRGBA(S->format,0,0,0,0));
-	
-	SDL_SetColorKey(Mapa, SDL_SRCCOLORKEY, 
-	  SDL_MapRGB(Mapa->format, 0, 0, 255));
-
-	SDL_BlitSurface(Mapa, NULL, S, &src1);			// And finally blit and update
-	SDL_UpdateRects(S, 1, &src1);*/
-
 	if (escenario.loaded()){
-		minimapa.ejecutar(S);
+		minimapa.ejecutar(S);		
 	}
-
+	ABC.ImprimirFrase(S,MensajeAMostrar);
 }
