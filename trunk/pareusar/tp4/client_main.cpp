b50426e2df5dbb6508f1d6e6c5543c884ec7a6b1
dnietoc <dnietoc@440cd090-4c3e-0410-b77e-8701ce9d1544>
2008-03-12 19:21:42 +0000

    Preparo el tronco para organizar ramas.

--- trunk/pareusar/tp4/client_main.cpp
+++
@@ -1,7 +0,0 @@
-#include "client_App.h"
-
-int main( int argc, char** argv )
-{
-    App theApp( argc, argv );
-    return theApp.run();
-}
--- trunk/server/memoria.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-#Esto ejecuta el valgrind con todos estas cosas sobre el programa pasado por el primer parametro
-
-exec valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes $1
